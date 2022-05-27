c14cde8dcf071961cfc81fae7d9930c8d2f4f1e4
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2022-05-27 16:26:58 +0900

    Strudy programmers VowelsDictionary
    Strudy programmers VowelsDictionary

--- src/Programmers/Level2/VowelsDictionary.java
+++ src/Programmers/Level2/VowelsDictionary.java
@@ -1,30 +1,27 @@
 package Programmers.Level2;
+
 // https://programmers.co.kr/learn/courses/30/lessons/84512
+
+//String[] wordArr = {"A", "E", "I", "O", "U"}; 리스트 인덱스찾기와 switch 중 뭐가 효율적인지 찾기
 
 public class VowelsDictionary {
 	public static int solution(String word) {
 		int answer = 0;
 		int wordLeng = word.length();
+		
 		String aReplace = word.replaceAll("A", "");
 		int aCheck = wordLeng - aReplace.length();
 		if(aCheck == wordLeng) return wordLeng;
 		
 		for(int i = 0 ; i <wordLeng; i++) {
-			System.out.println(answer);
-			if(wordLeng  < 5-i) {
-				answer=1;
-				answer *= 5 * (i+1) + 1;
-				continue;
-			}
-			
-			
-			
-			char now = word.charAt(wordLeng-i);
+			int sum = 1;
+			char now = word.charAt(i);
 			int seq = 5;
 			switch (now) {
 				case 'A': 
 					seq = 0;
-					break;
+					answer += 1;
+					continue;
 				
 				case 'E' :
 					seq = 1;
@@ -43,20 +40,25 @@ public class VowelsDictionary {
 					break;	
 			}
 			
-			answer *= 6 * seq + 1;
+			for(int j = 0; j < 4-i; j++) {
+				sum = 5 * sum + 1;
+			}
 			
+			sum = sum * seq +1;
+			answer += sum;
 		}
+		
 		return answer;
 	}
+	
 	public static void main(String[] args) {
-		String word = "AAAE";
+		String word = "AAAAE";
 		int result =solution(word);
 		
 		System.out.println(result);
 		
 	}
-	// AAAAA 5가지
-	//  5* 5* 5* 5
+	
 	
 	/*	'A', 'E', 'I', 'O', 'U
 	 * 	word	result
@@ -66,133 +68,5 @@ public class VowelsDictionary {
 		"EIO"	1189  782 + 313 + 94
 	 */
 	
-	/*
-	 * A X
-	 *   A X
-	 * 	   A X             -- 3
-	 *       A X A E I O U -- 9
-	 *       E X A E I O U -- 15
-	 *       I X A E I O U -- 21
-	 *       O X A E I O U -- 27
-	 *       U X A E I O U -- 33 
-	 *     E X			   -- 34
-	 *       A X A E I O U -- 40
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 64
-	 *     I X			   -- 65
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 95
-	 *     O X			   -- 96
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 126
-	 *     U X			   -- 127
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 157  150 + 5 + 1 + 1
-	 *   E X               -- 158
-	 *     A X             -- 159
-	 *       A X A E I O U -- 165
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 189
-	 *     E X             -- 190
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 220
-	 *     I X             -- 221
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 251
-	 *     O X
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U
-	 *     U X
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 313  150 + 150 + 5 + 5 + 1 + 1 + 1 
-	 *   I X
-	 * 	   A X             
-	 *       A X A E I O U 
-	 *       E X A E I O U 
-	 *       I X A E I O U 
-	 *       O X A E I O U 
-	 *       U X A E I O U  
-	 *     E X			   
-	 *       A X A E I O U 
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *     I X			  
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U
-	 *     O X			  
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *     U X			   
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U -- 313 + 150 + 5 + 1 = 469
-	 *   O X               
-	 *     A X            
-	 *       A X A E I O U 
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *     E X            
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *     I X            
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *     O X
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U
-	 *     U X
-	 *       A X A E I O U
-	 *       E X A E I O U
-	 *       I X A E I O U
-	 *       O X A E I O U
-	 *       U X A E I O U 
-	 *   
-	 *    
-	 */
+	
 }
