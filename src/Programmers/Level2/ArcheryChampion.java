fb03a2a3902874fb8172b6ab96be540977cb7d11
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2022-05-31 08:13:08 +0900

    Study programmers ArcheryChampion
    Study programmers ArcheryChampion

--- src/Programmers/Level2/ArcheryChampion.java
+++ src/Programmers/Level2/ArcheryChampion.java
@@ -5,7 +5,7 @@ public class ArcheryChampion {
 	public static int[] solution(String[] info, String[] query) {
 		int[] answer = new int[query.length];
 		
-		for(int i = 0;)
+		//for(int i = 0;)
 		return answer;
 	}
 
